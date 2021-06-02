d721e3689dfde1836f5c7190114741d02a29258e
Toranova <chia_jason96@live.com>
2021-06-03 04:35:31 +0800

    include eventlet worker flag for socketio

--- debug.sh
+++ debug.sh
@@ -3,3 +3,6 @@
 # runs the flask project with gunicorn, please ensure in virtualenv!
 # on localhost port 4000 with 4 worker threads.
 gunicorn -w 4 -b 127.0.0.1:4000 "project_name:create_app()"
+
+# if using eventlet (socketio)
+#gunicorn --worker-class eventlet -w 1 -b 127.0.0.1:5000 "ocrweb:create_app()"
